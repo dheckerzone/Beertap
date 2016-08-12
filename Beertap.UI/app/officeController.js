842f102ede835c5c70046e78ef048cc687faeaf1
Dherick.Lubigan <Dherick.Lubigan@MAN4-W541-W8>
2016-08-12 16:48:40 +0800

    UI - Done with Get Beer, Replace Keg

--- Beertap.UI/app/officeController.js
+++ Beertap.UI/app/officeController.js
@@ -19,5 +19,37 @@
                     });
                 }
 
+                $scope.getBeer = function (beer,index) {
+                    beer.Milliliters = 400;
+
+                    var link = appSettings.serverPath + beer._links.GetBeer.href;
+
+                    $http({
+                        method: "POST",
+                        url: link,
+                        headers: {
+                            'accept': 'application/hal+json',
+                            'Content-Type': 'application/json'
+                        },
+                        data: beer
+                    }).then(function (response) {
+                        $scope.beers._embedded.self[index] = response.data;
+                    });
+                }
+
+                $scope.replaceKeg = function (beer,index) {
+                    var link = appSettings.serverPath + beer._links.ReplaceKeg.href;
+                    $http({
+                        method: "POST",
+                        url: link,
+                        headers: {
+                            'accept': 'application/hal+json',
+                            'Content-Type': 'application/json'
+                        },
+                        data: beer
+                    }).then(function (response) {
+                        $scope.beers._embedded.self[index] = response.data;
+                    });
+                }
             });
 })();
